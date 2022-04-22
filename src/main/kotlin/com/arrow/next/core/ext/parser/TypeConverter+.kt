c139616b46603eeca2d9d50a23990882d7b1e80d
Moon <er4goon@gmail.com>
2022-04-22 15:37:09 +0700

    Benchmark

--- src/main/kotlin/com/arrow/next/core/ext/parser/TypeConverter+.kt
+++ src/main/kotlin/com/arrow/next/core/ext/parser/TypeConverter+.kt
@@ -13,11 +13,15 @@ import org.koin.core.context.GlobalContext.get
 import java.io.File
 import java.io.FileNotFoundException
 import java.io.FileReader
+import java.text.DecimalFormat
+import kotlin.math.log10
+import kotlin.math.pow
 
 
 typealias  PairLang = HashMap<String, LinkedTreeMap<String, String>>?
 val gson = get().get(named("bgson")) as Gson
 var moshi = get().get(named("bmoshi")) as Moshi
+private var _pairLang : PairLang? = null
 
 inline fun <reified T> toType(
     rawType: Class<*>,
@@ -113,7 +117,6 @@ inline fun <reified T> toListOfType(
 @Deprecated("Will change stream reader")
 private val file = get().get(named("file")) as File
 
-private var _pairLang : PairLang? = null
 val pairLang: PairLang
     get() {
         if(_pairLang == null) {
@@ -130,14 +133,45 @@ val pairLang: PairLang
 
 
 fun language(reload: Boolean = false): PairLang {
-    return if (reload) {
-        _pairLang = null
-        pairLang
-    } else {
-        pairLang
+    return when (reload) {
+        true -> {
+            _pairLang = null
+            pairLang
+        }
+        else -> {
+            pairLang
+        }
     }
 }
 
-fun translate(key: String, next: String): String {
+fun translate (key: String, next: String): String {
     return pairLang!![key]?.get(next)!!
+}
+
+
+fun benchmark (f: () -> Unit) {
+    System.gc()
+    val startTime = System.currentTimeMillis()
+    val startMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()
+
+    f.invoke()
+
+    System.gc()
+    val finishTime = System.currentTimeMillis()
+    val finishMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()
+
+    val elapsedMemory = finishMemory - startMemory
+    val elapsedTime = finishTime - startTime
+
+    println("Exe Process Time Consume: $elapsedTime")
+    println("Exe Process Used memory: ${elapsedMemory.formatSize}")
+}
+
+
+val Long.formatSize : String
+    get() {
+        if (this <= 0) return "0"
+        val units = arrayOf("B", "kB", "MB", "GB", "TB")
+        val digitGroups = (log10(this.toDouble()) / log10(1024.0)).toInt()
+        return DecimalFormat("#,##0.#").format(this / 1024.0.pow(digitGroups.toDouble())).toString() + " " + units[digitGroups]
 }
